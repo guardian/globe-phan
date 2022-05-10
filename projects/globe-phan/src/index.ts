7a8c3354ae4b471eb5531221d43ee9b6b4be0e04
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-05-10 18:16:07 +0100

    Update stack; normalise data

--- projects/globe-phan/src/index.ts
+++ projects/globe-phan/src/index.ts
@@ -1,23 +1,105 @@
-import fs from "fs";
 import { APIGatewayProxyResult } from "aws-lambda";
 import { createOkResponse } from "./lib/response";
 import { putDataIntoS3Bucket } from "./lib/util";
+import ophanData from "../ophanData.json";
 
-const getOphanData = async () => {
-  return JSON.parse(fs.readFileSync("./ophanData.json", "utf-8"));
+const bucketAtIndex = (ophanData: OphanData, index: number) =>
+  ophanData.aggregations.snapshots_over_time.buckets[index];
+
+const getNormalisedValue = (
+  location: string,
+  value: number,
+  minMaxMap: MinMaxMap
+) => {
+  const { min, max } = minMaxMap[location];
+  const range = max - min;
+  const singlePercent = 100 / range;
+  return ((value - min) * singlePercent) / 100;
+};
+
+type MinMaxMap = Record<string, { min: number; max: number }>;
+
+const processOphanData = (ophanData: OphanData) => {
+  const minMaxMap: MinMaxMap = {};
+  for (
+    let i = 0;
+    i < ophanData.aggregations.snapshots_over_time.buckets.length;
+    i++
+  ) {
+    const { key_as_string, key, doc_count, ...locations } = bucketAtIndex(
+      ophanData,
+      i
+    );
+
+    Object.entries(locations).map(([locationName, value]) => {
+      const docCount = value.buckets[0].doc_count;
+      const locationMinMax = minMaxMap[locationName];
+      if (locationMinMax) {
+        if (locationMinMax.max < docCount) {
+          locationMinMax.max = docCount;
+        }
+        if (locationMinMax.min > docCount) {
+          locationMinMax.min = docCount;
+        }
+      } else {
+        minMaxMap[locationName] = {
+          min: docCount,
+          max: docCount,
+        };
+      }
+    });
+  }
+
+  const latestBucketIndex =
+    ophanData.aggregations.snapshots_over_time.buckets.length - 1;
+
+  const { key_as_string, key, doc_count, ...locations } = bucketAtIndex(
+    ophanData,
+    latestBucketIndex
+  );
+
+  return Object.fromEntries(
+    Object.entries(locations).map(([locationName, value]) => [
+      locationName,
+      getNormalisedValue(locationName, value.buckets[0].doc_count, minMaxMap),
+    ])
+  );
+};
+
+type OphanData = {
+  aggregations: {
+    snapshots_over_time: {
+      buckets: Array<
+        {
+          key_as_string: string;
+          key: number;
+          doc_count: number;
+        } & Record<
+          string,
+          {
+            buckets: Array<{
+              doc_count: number;
+            }>;
+          }
+        >
+      >;
+    };
+  };
 };
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
   console.info("Globe-phan lambda invoked");
 
-  const ophanData = await getOphanData();
+  const outputData = processOphanData(ophanData as any);
 
-  await putDataIntoS3Bucket(JSON.stringify(ophanData), "data.json");
+  await putDataIntoS3Bucket(JSON.stringify(outputData), "data.json");
 
   console.info(`Written Ophan data to bucket`);
 
+  console.log(outputData);
+
   return {
     statusCode: 201,
-    body: JSON.stringify(createOkResponse(JSON.stringify(ophanData))),
+    body: JSON.stringify(createOkResponse(JSON.stringify(outputData))),
   };
 };
