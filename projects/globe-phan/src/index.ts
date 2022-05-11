d33507f35241ccfe74ddc23c1addec679f2e0044
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-05-11 14:47:35 +0100

    Update to fit ophan response

--- projects/globe-phan/src/index.ts
+++ projects/globe-phan/src/index.ts
@@ -3,113 +3,58 @@ import { APIGatewayProxyResult } from "aws-lambda";
 import { createOkResponse } from "./lib/response";
 import { putDataIntoS3Bucket } from "./lib/util";
 
-const bucketAtIndex = (ophanData: OphanData, index: number) =>
-  ophanData.aggregations.snapshots_over_time.buckets[index];
-
-const getNormalisedValue = (
-  location: string,
-  value: number,
-  minMaxMap: MinMaxMap
-) => {
-  const { min, max } = minMaxMap[location];
-  const range = max - min;
-  const singlePercent = 100 / range;
-  return ((value - min) * singlePercent) / 100;
-};
-
 type MinMaxMap = Record<string, { min: number; max: number }>;
 
-const fetchOphanData = () =>
+const fetchOphanData = (): Promise<OphanData> =>
   new Promise((resolve, reject) => {
-    const req = https.request(
-      {
-        hostname: "api.ophan.co.uk",
-        port: 443,
-        path: "/api/map",
-      },
+    https.get(
+      "https://api.ophan.co.uk/api/glophan/data?mins=10&key=NZFkS9fo6tFuAlRu",
       (response) => {
+        console.log({ response });
         let body = "";
-        response.on("data", (acc) => (body += acc));
+        response.on("data", (acc) => {
+          console.log("data");
+          body += acc;
+        });
         response.on("error", reject);
-        response.on("end", () => resolve(body));
+        response.on("end", () => resolve(JSON.parse(body)));
       }
     );
   });
 
 const processOphanData = (ophanData: OphanData) => {
-  const minMaxMap: MinMaxMap = {};
-  for (
-    let i = 0;
-    i < ophanData.aggregations.snapshots_over_time.buckets.length;
-    i++
-  ) {
-    const { key_as_string, key, doc_count, ...locations } = bucketAtIndex(
-      ophanData,
-      i
-    );
+  let min = 0;
+  let max = 0;
+  ophanData.forEach(({ hits }) => {
+    if (max < hits) {
+      max = hits;
+    }
+    if (min > hits) {
+      min = hits;
+    }
+  });
 
-    Object.entries(locations).map(([locationName, value]) => {
-      const docCount = value.buckets[0].doc_count;
-      const locationMinMax = minMaxMap[locationName];
-      if (locationMinMax) {
-        if (locationMinMax.max < docCount) {
-          locationMinMax.max = docCount;
-        }
-        if (locationMinMax.min > docCount) {
-          locationMinMax.min = docCount;
-        }
-      } else {
-        minMaxMap[locationName] = {
-          min: docCount,
-          max: docCount,
-        };
-      }
-    });
-  }
+  const range = max - min;
+  const singlePercent = 100 / range;
 
-  const latestBucketIndex =
-    ophanData.aggregations.snapshots_over_time.buckets.length - 1;
-
-  const { key_as_string, key, doc_count, ...locations } = bucketAtIndex(
-    ophanData,
-    latestBucketIndex
-  );
-
-  return Object.fromEntries(
-    Object.entries(locations).map(([locationName, value]) => [
-      locationName,
-      getNormalisedValue(locationName, value.buckets[0].doc_count, minMaxMap),
-    ])
-  );
-};
-
-type OphanData = {
-  aggregations: {
-    snapshots_over_time: {
-      buckets: Array<
-        {
-          key_as_string: string;
-          key: number;
-          doc_count: number;
-        } & Record<
-          string,
-          {
-            buckets: Array<{
-              doc_count: number;
-            }>;
-          }
-        >
-      >;
+  return ophanData.reduce((acc, { hits, location }) => {
+    return {
+      ...acc,
+      [location]: (hits * singlePercent) / 100,
     };
-  };
+  }, {} as Record<string, number>);
 };
+
+type OphanData = Array<{ location: string; hits: number }>;
 
 export const handler = async (): Promise<APIGatewayProxyResult> => {
   console.info("Globe-phan lambda invoked");
 
   const ophanData = await fetchOphanData();
 
-  const outputData = processOphanData(ophanData as any);
+  console.log({ ophanData });
+
+  const outputData = processOphanData(ophanData);
 
   await putDataIntoS3Bucket(JSON.stringify(outputData), "data.json");
 
